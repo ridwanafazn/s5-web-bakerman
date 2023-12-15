a18cdba4cff9ddca0f2788c2bb4cceae20e5f5cd
Ridwan Ahmad Fauzan <ridwanafzn@gmail.com>
2023-12-15 08:51:10 +0700

    categry pages

--- client/src/pages/Category.jsx
+++ client/src/pages/Category.jsx
@@ -2,31 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/category.css';
 
+import breadsImage from '../assets/images/category/breads.jpg';
+import cakesImage from '../assets/images/category/cakes.webp';
+import iceboxImage from '../assets/images/category/icebox.jpg';
+import cookiesImage from '../assets/images/category/cookies.jpg';
+import donutsImage from '../assets/images/category/donuts.jpg';
+import allProductsImage from '../assets/images/category/all.jpg';
+import bannerCategory from '../assets/images/category/banner-category.jpg';
+
 const categories = [
-  { name: 'Breads', link: '/category/breads' },
-  { name: 'Cakes', link: '/category/cakes' },
-  { name: 'Icebox Desserts', link: '/category/icebox-desserts' },
-  { name: 'Cookies', link: '/category/cookies' },
-  { name: 'Donuts', link: '/category/donuts' },
+  { name: 'Breads', link: '/category/breads', imageSrc: breadsImage },
+  { name: 'Cakes', link: '/category/cakes', imageSrc: cakesImage },
+  { name: 'Icebox Desserts', link: '/category/icebox-desserts', imageSrc: iceboxImage },
+  { name: 'Cookies', link: '/category/cookies', imageSrc: cookiesImage },
+  { name: 'Donuts', link: '/category/donuts', imageSrc: donutsImage },
+  { name: 'All Products', link: '/all', imageSrc: allProductsImage },
 ];
 
 const Category = () => {
   return (
     <div className="category-container">
-      <h1>Category</h1>
       <div className="hero-banner">
-        {/* Your hero banner content goes here */}
+        <img src={bannerCategory} alt="Banner" />
       </div>
-      <div className="category-boxes">
+
+      <div className="category-grid">
         {categories.map((category) => (
-          <Link key={category.name} to={category.link} className="category-box">
-            {category.name}
+          <Link key={category.name} to={category.link} className="category-item">
+            <img src={category.imageSrc} alt={category.name} />
+            <p>{category.name}</p>
           </Link>
         ))}
       </div>
-      <Link to="/category/all" className="show-all-products">
-        Show All Products
-      </Link>
     </div>
   );
 };
