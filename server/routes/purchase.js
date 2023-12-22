876c7923d802d7a37cd9de07be2a1628abce2f5c
Ridwan Ahmad Fauzan <ridwanafzn@gmail.com>
2023-12-22 09:04:42 +0700

    .

--- server/routes/purchase.js
+++
@@ -1,9 +0,0 @@
-// routes/purchase.js
-const express = require('express');
-const router = express.Router();
-const purchaseController = require('../controllers/purchaseController');
-
-router.post('/generate-code', purchaseController.generateUniqueCode);
-router.post('/redeem-purchase', purchaseController.redeemPurchase);
-
-module.exports = router;
